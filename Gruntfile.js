701e6b96161b909249b99f723d465d976277892b
feliun <felipe.polo@gmail.com>
2015-03-21 18:01:13 +0000

    Tidying up grunt dependencies to run the build in heroku

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ var mountFolder = function (connect, dir) {
 };
 
 module.exports = function (grunt) {
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    require('matchdep').filterAll('grunt-*').forEach(grunt.loadNpmTasks);
 
     grunt.initConfig({
         watch: {
