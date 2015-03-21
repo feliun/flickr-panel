9de59b1372554bf3c40124bc5d61264059f35101
feliun <felipe.polo@gmail.com>
2015-03-21 22:20:17 +0000

    Including routing based on states

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,32 @@
 (function() {
-	var app = angular.module('flickrApp', [ 'httpServices',
+	var app = angular.module('flickrApp', [ 'ui.router',
+											'httpServices',
 											'navigationCtrls', 'filterCtrls', 'picBoardCtrls',
-											'navigationDirectives', 'filterDirectives', 'picturesDirectives'])
+											'navigationDirectives', 'filterDirectives', 'picturesDirectives' ])
 				.controller('MainCtrl', function() {
 					
+				})
+				.config(function($stateProvider, $urlRouterProvider) {
+					$urlRouterProvider.otherwise("/");
+					$stateProvider
+						.state('filter', {
+							url: "/",
+							views: {
+				                "mainView": {
+				                    templateUrl: "../html/filter-selector.html",
+				                    controller: "FilterCtrl"
+				                }
+				            }
+						})
+						.state('result', {
+							url: "/result",
+							views: {
+				                "mainView": {
+				                    templateUrl: "../html/pictures-board.html",
+				                    controller: "PicBoardCtrl"
+				                }
+				            }
+						});
 				});
+
 })();
