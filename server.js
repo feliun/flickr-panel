6a3e39074085ceb96329e14f0364d643e4446161
feliun <felipe.polo@gmail.com>
2015-03-21 18:20:00 +0000

    Removing PORT from the .env file to let Heroku inject it

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
 	res.redirect('index.html');
 });
-var port = process.env.PORT || 4000;
+var port = process.env.PORT || 5000;
 app.listen(port, '127.0.0.1', function(err) {
     if (err) return console.err('Error: ', err.stack);
     console.log('Listening on port ' + port);
