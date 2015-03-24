1d5629491832bd84d809f4eef0ef116797b5dc73
feliun <felipe.polo@gmail.com>
2015-03-24 11:25:15 +0000

    Fixing issue when loading images after being in the loading page

--- public/js/controllers/board.js
+++ public/js/controllers/board.js
@@ -1,16 +1,13 @@
 (function() {
 	var app = angular.module('picBoardCtrls', ['httpServices'])
 				.controller('PicBoardCtrl', ['$scope', 'HttpQueriesSvc', function($scope, httpQueries) {
-					$scope.picsList = [];
-					$scope.picsList = httpQueries.getPics();
 
-					$scope.$apply(function() {
-						pics = _.map(pics, function(pic) {
-							pic.tags = pic.tags.split(' ');
-							pic.score = Math.floor(Math.random() * 6);
-							return pic;
-						});
-						$scope.picsList = pics;
+					$scope.picsList = httpQueries.getPics();
+					
+					$scope.picsList = _.map($scope.picsList, function(pic) {
+						pic.tags = pic.tags.split(' ');
+						pic.score = Math.floor(Math.random() * 6);
+						return pic;
 					});
 
 				    $scope.getArrayFromNumber = function(num) {
