b6fa6a0ba00009ffd6e538f9c3f701ca2920e773
feliun <felipe.polo@gmail.com>
2015-03-28 15:22:50 +0000

    Displaying error page when no results are found

--- public/js/controllers/board.js
+++ public/js/controllers/board.js
@@ -2,7 +2,9 @@
 	var app = angular.module('picBoardCtrls', ['httpServices'])
 				.controller('PicBoardCtrl', ['$scope', 'HttpQueriesSvc', function($scope, httpQueries) {
 
+					$scope.validResult = true;
 					$scope.picsList = httpQueries.getPics();
+					if ($scope.picsList.length === 0) $scope.validResult = false;
 					
 					$scope.picsList = _.map($scope.picsList, function(pic) {
 						pic.tags = pic.tags.split(' ');
