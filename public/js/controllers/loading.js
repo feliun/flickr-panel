6d7d1227c7df65c758cc3c691a628995b025cdd4
feliun <felipe.polo@gmail.com>
2015-03-28 16:04:56 +0000

    Being more defensive in case the events with pictures takes a long time to be triggered

--- public/js/controllers/loading.js
+++ public/js/controllers/loading.js
@@ -4,11 +4,16 @@
 											function($scope, $rootScope, $state, $interval, $timeout, httpQueries) {
 
 					var COURTESY_DELAY = 1000;
+					var MAX_TIME = 3000;
 
 					$scope.$on('newPics', function(event, pics) {
 						$timeout(function() {
 							$state.go("result");
 						}, COURTESY_DELAY);
 				    });
+
+				    $timeout(function() {
+							$state.go("result");
+					}, MAX_TIME);
 				}]);
 })();
